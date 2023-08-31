a9e1eef5856da826dfe31540d484fc454b5b64e0
sajjaddg <sajjaddg@gmail.com>
2023-08-31 22:17:17 +0330

    change

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,34 +1,39 @@
-import Ffmpeg from 'fluent-ffmpeg'
-import { renameSync, unlinkSync } from 'fs'
-import { IMetadata, ITrack } from '../typings'
+import Ffmpeg from 'fluent-ffmpeg';
+import * as FileSystem from 'expo-file-system';
+import {IMetadata, ITrack} from '../typings';
 
 export default async (data: ITrack, file: string): Promise<string> => {
-    const outputOptions: string[] = ['-map', '0:0', '-codec', 'copy']
+    const outputOptions: string[] = ['-map', '0:0', '-codec', 'copy'];
 
     const metadata: IMetadata = {
         title: data.name,
         album: data.album_name,
         artist: data.artists,
-        date: data.release_date
+        date: data.release_date,
         //attachments: []
-    }
+    };
 
     Object.keys(metadata).forEach((key) => {
-        outputOptions.push('-metadata', `${String(key)}=${metadata[key as 'title' | 'artist' | 'date' | 'album']}`)
-    })
+        outputOptions.push('-metadata', `${String(key)}=${metadata[key as 'title' | 'artist' | 'date' | 'album']}`);
+    });
 
-    const out = `${file.split('.')[0]}_temp.mp3`
+    const out = `${file.split('.')[0]}_temp.mp3`;
+
     await new Promise((resolve, reject) => {
         Ffmpeg()
             .input(file)
             .on('error', (err) => {
-                reject(err)
+                reject(err);
             })
-            .on('end', () => resolve(file))
+            .on('end', async () => {
+                await FileSystem.moveAsync({from: out, to: file}); // Using Expo File System's moveAsync
+                resolve(file);
+            })
             .addOutputOptions(...outputOptions)
-            .saveToFile(out)
-    })
-    unlinkSync(file)
-    renameSync(out, file)
-    return file
-}
+            .saveToFile(out);
+    });
+
+    await FileSystem.deleteAsync(file); // Using Expo File System's deleteAsync
+
+    return file;
+};
